0404e2effce30224c1a09b41da20b9750260f33c
Rene Richarz <rene.richarz@bluewin.ch>
2021-01-25 14:39:33 +0100

    Cygwin defines FIONREAD in <sys/socket.h>. #26

--- tube.c
+++ tube.c
@@ -48,6 +48,7 @@
 #include <sys/time.h>
 #include <locale.h>
 #include <pwd.h>
+// Cygwin defines FIONREAD in <sys/socket.h>. #26
 #ifndef FIONREAD
 #include <sys/socket.h>
 #endif
@@ -287,7 +288,7 @@ void tube_init(int argc, char* argv[])
         char *argv2[20];
         size_t bufsize = 127;
         int firstArg = 1;
-        printf("tek4010 version 1.5.7b\n");
+        printf("tek4010 version 1.6\n");
         windowName = "Tektronix 4010/4014 emulator";
         if ((argc<2) || (argc>19)) {
                 printf("Error:number of arguments\n");
